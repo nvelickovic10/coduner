cd150e9464a911c68eb06f28c8ee50158cc5d8c2
Wrath Amon <>
2018-11-10 00:10:20 +0100

    no message

--- src/coduner-executor/server/index.js
+++ src/coduner-executor/server/index.js
@@ -1,31 +1,10 @@
 import express from 'express';
 const { VM } = require('vm2');
-import fs from 'fs';
-
-import arrayUtil from './util/arrayUtil';
 
 const app = express();
 const vm = new VM();
 
-const publicDir = './public';
-const galleryDir = `${publicDir}/resources/gallery/`;
-
-app.use(express.static(publicDir));
-
-app.get('/', (req, res) => {
-    res.sendfile(`${publicDir}/index.html`);
-});
-
-app.get('/images', (req, res) => {
-    fs.readdir(galleryDir, (err, files) => {
-        for (let i = 0; i < files.length; i++) {
-            files[i] = `resources/gallery/${files[i]}`;
-        }
-        res.send(arrayUtil.shuffle(files));
-    })
-});
-
-app.post('/execjs', (req, res) => {
+app.post('/', (req, res) => {
     res.send(200, vm.run('function a() {return 4;}; setTimeout(2000, a);'));
 });
 
